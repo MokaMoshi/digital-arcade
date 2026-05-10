8cb20ff16b5113e0a5c624131d1409a2f1daa6e4
MokaMoshi <fc.araya@duocuc.cl>
2026-05-09 21:36:54 -0400

    Proyecto movido a carpeta local y listo para trabajar

---
+++ src/main/java/com/digital_arcade_spring/digital_arcade/DigitalArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.digital_arcade_spring.digital_arcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalArcadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalArcadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital_arcade_spring/digital_arcade/DigitalArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_arcade_spring.digital_arcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalArcadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
